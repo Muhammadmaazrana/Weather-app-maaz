7623a72d449d931b6266dfc285c575b3e523b0ea
MUHAMMAD MAAZ RANA <80382672+Muhammadmaazrana@users.noreply.github.com>
2023-07-29 21:08:16 +0500

    work on geo location

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import './App.css'; // Make sure you have an App.css file with the provided CSS styles
 
 function App() {
   const [weather, setWeather] = useState(null);
-  const [city, setCity] = useState('');
+  const [query, setQuery] = useState('');
 
-  const fetchWeatherData = async (latitude, longitude) => {
+  const fetchWeatherData = async (input) => {
     try {
       // Replace 'YOUR_API_KEY' with your actual API key from OpenWeatherMap
       const apiKey = '81166c5917e725eed05d9bd2ce435358';
       const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
+        `https://api.openweathermap.org/data/2.5/weather?${input}&appid=${apiKey}&units=metric`
       );
       if (!response.ok) {
         throw new Error('City not found');
@@ -22,31 +23,47 @@ function App() {
     }
   };
 
-  const handleCityChange = (event) => {
-    setCity(event.target.value);
+  const handleInput = (event) => {
+    setQuery(event.target.value);
   };
 
   const handleSearch = () => {
-    fetchWeatherDataForCity();
+    fetchWeatherData(`q=${query}`);
   };
 
-  const fetchWeatherDataForCity = () => {
-    if (city.trim() === '') return;
-    fetchWeatherData(null, null); // Reset previous location data
-    setCity(city.trim());
+  const getLocationWeather = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+          fetchWeatherData(`lat=${latitude}&lon=${longitude}`);
+        },
+        (error) => {
+          console.error(error);
+          setWeather(null);
+        }
+      );
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+    }
   };
 
-  // List of background images (add your own image URLs here)
+  useEffect(() => {
+    getLocationWeather();
+  }, []); // Fetch weather data on initial load based on user's geolocation
+
+  // List of background images
   const backgroundImages = [
     'url("https://images.pexels.com/photos/6195003/pexels-photo-6195003.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")',
     'url("https://images.unsplash.com/photo-1530908295418-a12e326966ba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80")',
-    'url("https://images.unsplash.com/photo-1562494400-5b335a653209?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=462&q=80)',
+    'url("https://images.unsplash.com/photo-1562494400-5b335a653209?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=462&q=80")',
     // Add more image URLs as needed
   ];
 
   const [backgroundIndex, setBackgroundIndex] = useState(0);
 
-  // Function to change the background image every 3 seconds
+  // Function to change the background image every 7 seconds
   useEffect(() => {
     const interval = setInterval(() => {
       setBackgroundIndex((prevIndex) =>
@@ -63,29 +80,6 @@ function App() {
     document.body.style.backgroundImage = backgroundImages[backgroundIndex];
   }, [backgroundIndex]);
 
-  useEffect(() => {
-    if (!city) {
-      // Get geolocation and fetch weather data for user's location
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            const { latitude, longitude } = position.coords;
-            fetchWeatherData(latitude, longitude);
-          },
-          (error) => {
-            console.error('Error getting user location:', error);
-            // Fetch default weather data (e.g., for a city like New York)
-            fetchWeatherDataForCity();
-          }
-        );
-      } else {
-        console.error('Geolocation is not supported by your browser.');
-        // Fetch default weather data (e.g., for a city like New York)
-        fetchWeatherDataForCity();
-      }
-    }
-  }, [city]);
-
   return (
     <div className="background-div">
       <div className="container">
@@ -99,12 +93,15 @@ function App() {
                 <div className="input-group">
                   <input
                     type="text"
-                    className="form-control"
+                    className="form-control search-input"
                     placeholder="Enter city name..."
-                    value={city}
-                    onChange={handleCityChange}
+                    value={query}
+                    onChange={handleInput}
                   />
-                  <button className="btn btn-primary" onClick={handleSearch}>
+                  <button
+                    className="btn btn-primary search-btn"
+                    onClick={handleSearch}
+                  >
                     Search
                   </button>
                 </div>
