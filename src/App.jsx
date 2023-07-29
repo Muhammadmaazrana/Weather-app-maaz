ab8dedccb9394e284b3a8630f488928476872de2
MUHAMMAD MAAZ RANA <80382672+Muhammadmaazrana@users.noreply.github.com>
2023-07-29 13:26:41 +0500

    add geo location

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
-
 function App() {
   const [weather, setWeather] = useState(null);
   const [city, setCity] = useState('');
 
-  const fetchWeatherData = async () => {
+  const fetchWeatherData = async (latitude, longitude) => {
     try {
       // Replace 'YOUR_API_KEY' with your actual API key from OpenWeatherMap
       const apiKey = '81166c5917e725eed05d9bd2ce435358';
       const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
       );
       if (!response.ok) {
         throw new Error('City not found');
@@ -28,13 +27,18 @@ function App() {
   };
 
   const handleSearch = () => {
-    fetchWeatherData();
+    fetchWeatherDataForCity();
+  };
+
+  const fetchWeatherDataForCity = () => {
+    if (city.trim() === '') return;
+    fetchWeatherData(null, null); // Reset previous location data
+    setCity(city.trim());
   };
 
   // List of background images (add your own image URLs here)
   const backgroundImages = [
     'url("https://images.pexels.com/photos/6195003/pexels-photo-6195003.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")',
-    'url("https://images.pexels.com/photos/3227984/pexels-photo-3227984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")',
     'url("https://images.unsplash.com/photo-1530908295418-a12e326966ba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80")',
     'url("https://images.unsplash.com/photo-1562494400-5b335a653209?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=462&q=80)',
     // Add more image URLs as needed
@@ -59,6 +63,29 @@ function App() {
     document.body.style.backgroundImage = backgroundImages[backgroundIndex];
   }, [backgroundIndex]);
 
+  useEffect(() => {
+    if (!city) {
+      // Get geolocation and fetch weather data for user's location
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const { latitude, longitude } = position.coords;
+            fetchWeatherData(latitude, longitude);
+          },
+          (error) => {
+            console.error('Error getting user location:', error);
+            // Fetch default weather data (e.g., for a city like New York)
+            fetchWeatherDataForCity();
+          }
+        );
+      } else {
+        console.error('Geolocation is not supported by your browser.');
+        // Fetch default weather data (e.g., for a city like New York)
+        fetchWeatherDataForCity();
+      }
+    }
+  }, [city]);
+
   return (
     <div className="background-div">
       <div className="container">
@@ -66,46 +93,59 @@ function App() {
           <div className="col-lg-6 col-md-8 col-sm-10">
             <div className="card bg-transparent border-0">
               <div className="card-body">
-              <h1 className="text-center mb-4"><b>Weather App</b></h1>
-              <div className="input-group">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Enter city name..."
-                  value={city}
-                  onChange={handleCityChange}
-                />
-                <button className="btn btn-primary" onClick={handleSearch}>
-                  Search
-                </button>
-              </div>
-              <br />
-              {weather ? (
-                <div className="weather-info">
-                  <h2 className="text-center mb-4">
-                 <b>   {weather.name}, {weather.sys.country}</b>
-                  </h2>
-                  <div className="d-flex justify-content-center align-items-center weather-icon">
-                    <img
-                      src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
-                      alt="Weather Icon"
-                    />
-                    <h3 className="ms-3 weather-description"><b>
-                      {weather.weather[0].description}</b>
-                    </h3>
-                  </div>
-                  <div className="text-center mt-4 weather-details">
-                    <h1 className="display-1"><b>{weather.main.temp}°C</b></h1>
-                    <p><b>Feels like: {weather.main.feels_like}°C</b></p>
-                    <p><b>Humidity: {weather.main.humidity}%</b></p>
-                    <p><b>Wind: {weather.wind.speed} m/s</b></p>
-                  </div>
+                <h1 className="text-center mb-4">
+                  <b>Weather App</b>
+                </h1>
+                <div className="input-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter city name..."
+                    value={city}
+                    onChange={handleCityChange}
+                  />
+                  <button className="btn btn-primary" onClick={handleSearch}>
+                    Search
+                  </button>
                 </div>
-              ) : (
-                
-                <p className="text-center"><b>Enter a city to get the weather</b></p>
-              )}
-            </div>
+                <br />
+                {weather ? (
+                  <div className="weather-info">
+                    <h2 className="text-center mb-4">
+                      <b>
+                        {weather.name}, {weather.sys.country}
+                      </b>
+                    </h2>
+                    <div className="d-flex justify-content-center align-items-center weather-icon">
+                      <img
+                        src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+                        alt="Weather Icon"
+                      />
+                      <h3 className="ms-3 weather-description">
+                        <b>{weather.weather[0].description}</b>
+                      </h3>
+                    </div>
+                    <div className="text-center mt-4 weather-details">
+                      <h1 className="display-1">
+                        <b>{weather.main.temp}°C</b>
+                      </h1>
+                      <p>
+                        <b>Feels like: {weather.main.feels_like}°C</b>
+                      </p>
+                      <p>
+                        <b>Humidity: {weather.main.humidity}%</b>
+                      </p>
+                      <p>
+                        <b>Wind: {weather.wind.speed} m/s</b>
+                      </p>
+                    </div>
+                  </div>
+                ) : (
+                  <p className="text-center">
+                    <b>Enter a city to get the weather</b>
+                  </p>
+                )}
+              </div>
             </div>
           </div>
         </div>
